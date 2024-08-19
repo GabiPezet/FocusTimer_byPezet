aeeb419a1587b818ae329f4e9a1fe033d54c9ddb
Gabriel Pezet <gabriel.pzt@gmail.com>
2024-08-18 23:20:41 -0300

    project create - theme configuration and dependencies implementation

---
+++ app/src/main/java/com/example/focustimer/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.focustimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.focustimer.ui.theme.FocusTimerTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val windowSize = calculateWindowSizeClass(activity = this)
+            FocusTimerTheme(windowSize = windowSize.widthSizeClass) {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Gabriel",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hola $name!, estamos empezando un proyecto nuevo!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FocusTimerTheme {
+        Greeting("Android")
+    }
+}
